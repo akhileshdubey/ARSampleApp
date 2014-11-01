af9f80ec1bb5e6fcac2137b133b71eca4025fb4c
navkrishna <navkrishna@intelligrape.com>
2014-11-01 12:49:27 +0530

    Added transparency and positioned glass

--- app/src/main/java/com/example/intelligrape/arsampeapp/GLClearRenderer.java
+++ app/src/main/java/com/example/intelligrape/arsampeapp/GLClearRenderer.java
@@ -1,11 +1,15 @@
 package com.example.intelligrape.arsampeapp;
 
+import javax.microedition.khronos.egl.EGLConfig;
+import javax.microedition.khronos.opengles.GL10;
+
 import android.content.Context;
 import android.graphics.Bitmap;
-import android.opengl.GLSurfaceView.Renderer;
 import android.opengl.GLU;
+import android.opengl.GLSurfaceView.Renderer;
 import android.os.Environment;
 import android.util.Log;
+import android.view.MotionEvent;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
@@ -14,78 +18,47 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.IntBuffer;
 
-import javax.microedition.khronos.egl.EGLConfig;
-import javax.microedition.khronos.opengles.GL10;
-
 public class GLClearRenderer implements Renderer {
-
+	
+	private TextureCube mVSponser = new TextureCube();
     public int mResouceId = R.raw.intelligrape;
     public float mY = 8.0f;
     public float mX = 7.0f;
     public float mZ = -20.f;
-    public float mRotate = 0.0f;
+    public float mRotate = 90.0f;
     public int mDemoId = 0;
     public boolean mAutoRotate = true;
-    private TextureCube mVSponser = new TextureCube();
+
     private Context context;
 
 
-    GLClearRenderer(Context ctx) {
+    GLClearRenderer(Context ctx){
 
         context = ctx;
     }
 
-    //    void saveAlbum(GL10 gl)
-//    {
-//        Bitmap bmp = SavePixels(0, 0, windowWIDTH, windowHEIGHT, gl);  // display wid,height must be
-//        bmp = rotate(bmp,DefinallyROTATION);
-//        path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bmp, "hi", null);
-//        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_MEDIA_SCANNER_STARTED);
-//        intentFilter.addAction(Intent.ACTION_MEDIA_SCANNER_FINISHED);
-//        intentFilter.addDataScheme("file");
-//        context.registerReceiver(mReceiver, intentFilter);
-//        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://"
-//                + Environment.getExternalStorageDirectory())));
-//    }
-    public static Bitmap SavePixels(int x, int y, int w, int h, GL10 gl) {
-        int b[] = new int[w * (y + h)];
-        int bt[] = new int[w * h];
-        IntBuffer ib = IntBuffer.wrap(b);
-        ib.position(0);
-        gl.glReadPixels(x, 0, w, y + h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, ib);
-
-        for (int i = 0, k = 0; i < h; i++, k++) {//remember, that OpenGL bitmap is incompatible with Android bitmap
-            //and so, some correction need.
-            for (int j = 0; j < w; j++) {
-                int pix = b[i * w + j];
-                int pb = (pix >> 16) & 0xff;
-                int pr = (pix << 16) & 0x00ff0000;
-                int pix1 = (pix & 0xff00ff00) | pr | pb;
-                bt[(h - k - 1) * w + j] = pix1;
-            }
-        }
 
 
-        Bitmap sb = Bitmap.createBitmap(bt, w, h, Bitmap.Config.ARGB_8888);
-        return sb;
-    }
-
-    public void onDrawFrame(GL10 gl) {
+    public void onDrawFrame( GL10 gl ) {
         // This method is called per frame, as the name suggests.
         // For demonstration purposes, I simply clear the screen with a random translucent gray.
         //float c = 1.0f / 256 * ( System.currentTimeMillis() % 256 );
         //gl.glClearColor( c, c, c, 0.5f );
         //gl.glClear( GL10.GL_COLOR_BUFFER_BIT );
         gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+        gl.glClearColor(0.0f,0.0f,0.0f,0.0f);
         gl.glLoadIdentity();
         gl.glTranslatef(mX, mY, mZ);
-        gl.glRotatef(mRotate, 1.0f, 1.0f, 1.0f);
 
-        // gl.glScalef(2.0f,2.0f,2.0f);
-
-        mVSponser.draw(gl);
+        gl.glRotatef(mRotate, 0.0f, 0.0f, 1.0f);
 
 
+       // gl.glScalef(2.0f,2.0f,2.0f);
+
+       mVSponser.draw(gl);
+
+
+           
         gl.glLoadIdentity();
 
         if (mAutoRotate)
@@ -97,8 +70,8 @@ public class GLClearRenderer implements Renderer {
 //            CameraProjectActivity.isScreenshot=false;
 //
 //        }
-
-
+        
+        
     }
 
     private boolean storeImage(GL10 gl) {
@@ -116,7 +89,7 @@ public class GLClearRenderer implements Renderer {
             BufferedOutputStream bos = new BufferedOutputStream(fileOutputStream);
 
             //choose another format if PNG doesn't suit you
-            Bitmap imageData = SavePixels(0, 0, 400, 400, gl);
+            Bitmap imageData = SavePixels(0, 0, 400,400,gl);
             imageData.compress(Bitmap.CompressFormat.PNG, 100, bos);
 
             bos.flush();
@@ -133,33 +106,72 @@ public class GLClearRenderer implements Renderer {
         return true;
     }
 
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
+//    void saveAlbum(GL10 gl)
+//    {
+//        Bitmap bmp = SavePixels(0, 0, windowWIDTH, windowHEIGHT, gl);  // display wid,height must be
+//        bmp = rotate(bmp,DefinallyROTATION);
+//        path = MediaStore.Images.Media.insertImage(context.getContentResolver(), bmp, "hi", null);
+//        IntentFilter intentFilter = new IntentFilter(Intent.ACTION_MEDIA_SCANNER_STARTED);
+//        intentFilter.addAction(Intent.ACTION_MEDIA_SCANNER_FINISHED);
+//        intentFilter.addDataScheme("file");
+//        context.registerReceiver(mReceiver, intentFilter);
+//        context.sendBroadcast(new Intent(Intent.ACTION_MEDIA_MOUNTED, Uri.parse("file://"
+//                + Environment.getExternalStorageDirectory())));
+//    }
+    public static Bitmap SavePixels(int x, int y, int w, int h, GL10 gl)
+    {
+        int b[]=new int[w*(y+h)];
+        int bt[]=new int[w*h];
+        IntBuffer ib=IntBuffer.wrap(b);
+        ib.position(0);
+        gl.glReadPixels(x, 0, w, y+h, GL10.GL_RGBA, GL10.GL_UNSIGNED_BYTE, ib);
+
+        for(int i=0, k=0; i<h; i++, k++)
+        {//remember, that OpenGL bitmap is incompatible with Android bitmap
+            //and so, some correction need.
+            for(int j=0; j<w; j++)
+            {
+                int pix=b[i*w+j];
+                int pb=(pix>>16)&0xff;
+                int pr=(pix<<16)&0x00ff0000;
+                int pix1=(pix&0xff00ff00) | pr | pb;
+                bt[(h-k-1)*w+j]=pix1;
+            }
+        }
+
+
+        Bitmap sb=Bitmap.createBitmap(bt, w, h, Bitmap.Config.ARGB_8888);
+        return sb;
+    }
+ 
+    public void onSurfaceChanged( GL10 gl, int width, int height ) {
         // This is called whenever the dimensions of the surface have changed.
         // We need to adapt this change for the GL viewport.
-        gl.glViewport(0, 0, width, height);
+        gl.glViewport( 0, 0, width, height );
         //gl.glViewport(0, 0, width, height);
         gl.glMatrixMode(GL10.GL_PROJECTION);
         gl.glLoadIdentity();
-        GLU.gluPerspective(gl, 45.0f, (float) width / (float) height, 0.1f, 100.0f);
+        GLU.gluPerspective(gl, 45.0f, (float)width / (float)height, 0.1f, 100.0f);
         gl.glViewport(0, 0, width, height);
 
         gl.glMatrixMode(GL10.GL_MODELVIEW);
         gl.glLoadIdentity();
     }
-
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+ 
+    public void onSurfaceCreated( GL10 gl, EGLConfig config ) {
         // No need to do anything here.
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
-
+    	gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f); 
+        
         gl.glClearDepthf(1.0f);
         gl.glEnable(GL10.GL_DEPTH_TEST);
         gl.glDepthFunc(GL10.GL_LEQUAL);
 
         gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
-                GL10.GL_NICEST);
+                  GL10.GL_NICEST);
 
-        mVSponser.loadTexture(gl, context, mResouceId);    // Load image into Texture (NEW)
-        gl.glEnable(GL10.GL_TEXTURE_2D);
+       mVSponser.loadTexture(gl, context, mResouceId);    // Load image into Texture (NEW)
+
+       gl.glEnable(GL10.GL_TEXTURE_2D);
     }
 
 //    public Float[] GetWorldCoords(GL10 gl,float x, float y)
--- app/src/main/java/com/example/intelligrape/arsampeapp/VirtualSponser.java
+++ app/src/main/java/com/example/intelligrape/arsampeapp/VirtualSponser.java
@@ -16,95 +16,93 @@ import android.widget.Button;
 import android.widget.RelativeLayout;
 
 public class VirtualSponser extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+    /** Called when the activity is first created. */
 
     Integer[] imageIDs = {
             R.drawable.icon,
             R.drawable.mutan_mini,
 
     };
-    //    public static boolean invertPosition = false;
+//    public static boolean invertPosition = false;
 //    public static boolean isScreenshot = false;
 //    public static Bitmap bmp = null;
     GLClearRenderer glClearRenderer;
 
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
+    public void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+     
         // When working with the camera, it's useful to stick to one orientation.
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
-
+        setRequestedOrientation( ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE );
+     
         // Next, we disable the application's title bar...
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        requestWindowFeature( Window.FEATURE_NO_TITLE );
         // ...and the notification bar. That way, we can use the full screen.
-        getWindow().setFlags(LayoutParams.FLAG_FULLSCREEN,
-                LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags( LayoutParams.FLAG_FULLSCREEN,
+                                LayoutParams.FLAG_FULLSCREEN );
 
         // Now let's create an OpenGL surface.
-        GLSurfaceView glView = new GLSurfaceView(this);
+        GLSurfaceView glView = new GLSurfaceView( this );
         // To see the camera preview, the OpenGL surface has to be created translucently.
         // See link above.
-        glView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
-        glView.getHolder().setFormat(PixelFormat.TRANSLUCENT);
+        glView.setEGLConfigChooser( 8, 8, 8, 8, 16, 0 );
+        glView.getHolder().setFormat( PixelFormat.TRANSLUCENT );
         // The renderer will be implemented in a separate class, GLView, which I'll show next.
         glClearRenderer = new GLClearRenderer(this);
         glClearRenderer.mResouceId = this.getIntent().getExtras().getInt("resource_id");
-        glClearRenderer.mAutoRotate = true;
-        glView.setRenderer(glClearRenderer);
+        glClearRenderer.mAutoRotate= true;
+        glView.setRenderer(  glClearRenderer);
         glView.setZOrderOnTop(true);
         // Now set this as the main view.
-        setContentView(glView);
+        setContentView( glView );
 
-
+     
         // Now also create a view which contains the camera preview...
-        CameraView cameraView = new CameraView(this);
+        CameraView cameraView = new CameraView( this );
         // ...and add it, wrapping the full screen size.
-        addContentView(cameraView, new LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT));
+        addContentView( cameraView, new LayoutParams( LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT ) );
 
         RelativeLayout lContainerLayout = new RelativeLayout(this);
-        lContainerLayout.setLayoutParams(new RelativeLayout.LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        lContainerLayout.setLayoutParams(new RelativeLayout.LayoutParams( LayoutParams.FILL_PARENT , LayoutParams.FILL_PARENT ));
 
         lContainerLayout.addView(loadToolBox());
-        addContentView(lContainerLayout, new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.FILL_PARENT));
+        addContentView(lContainerLayout, new LayoutParams( LayoutParams.FILL_PARENT , LayoutParams.FILL_PARENT ) );
 
     }
 
 
-    private View loadToolBox() {
+    private View loadToolBox(){
 
-        RelativeLayout.LayoutParams lButtonParams = new RelativeLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT);
+        RelativeLayout.LayoutParams lButtonParams = new RelativeLayout.LayoutParams( LayoutParams.WRAP_CONTENT , LayoutParams.WRAP_CONTENT );
         lButtonParams.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM);
 
 
-        LayoutInflater loiViewInflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        LayoutInflater loiViewInflater = (LayoutInflater) getSystemService (Context.LAYOUT_INFLATER_SERVICE);
         loiViewInflater = LayoutInflater.from(getApplicationContext());
         View mToolBox = loiViewInflater.inflate(R.layout.toolbox, null);
-        ((Button) mToolBox.findViewById(R.id.left)).setOnClickListener(new View.OnClickListener() {
+        ((Button)mToolBox.findViewById(R.id.left)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 glClearRenderer.mX--;
             }
         });
-        ((Button) mToolBox.findViewById(R.id.right)).setOnClickListener(new View.OnClickListener() {
+        ((Button)mToolBox.findViewById(R.id.right)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 glClearRenderer.mX++;
 
             }
         });
-        ((Button) mToolBox.findViewById(R.id.up)).setOnClickListener(new View.OnClickListener() {
+        ((Button)mToolBox.findViewById(R.id.up)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 glClearRenderer.mY++;
 
             }
         });
-        ((Button) mToolBox.findViewById(R.id.down)).setOnClickListener(new View.OnClickListener() {
+        ((Button)mToolBox.findViewById(R.id.down)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -112,7 +110,7 @@ public class VirtualSponser extends Activity {
 
             }
         });
-        ((Button) mToolBox.findViewById(R.id.zoomIn)).setOnClickListener(new View.OnClickListener() {
+        ((Button)mToolBox.findViewById(R.id.zoomIn)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -120,14 +118,14 @@ public class VirtualSponser extends Activity {
 
             }
         });
-        ((Button) mToolBox.findViewById(R.id.zoomOut)).setOnClickListener(new View.OnClickListener() {
+        ((Button)mToolBox.findViewById(R.id.zoomOut)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 glClearRenderer.mZ--;
 
             }
         });
-        ((Button) mToolBox.findViewById(R.id.rLeft)).setOnClickListener(new View.OnClickListener() {
+        ((Button)mToolBox.findViewById(R.id.rLeft)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -135,7 +133,7 @@ public class VirtualSponser extends Activity {
 
             }
         });
-        ((Button) mToolBox.findViewById(R.id.rRight)).setOnClickListener(new View.OnClickListener() {
+        ((Button)mToolBox.findViewById(R.id.rRight)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -144,7 +142,7 @@ public class VirtualSponser extends Activity {
             }
         });
 
-        ((Button) mToolBox.findViewById(R.id.visitUs)).setOnClickListener(new View.OnClickListener() {
+        ((Button)mToolBox.findViewById(R.id.visitUs)).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -204,6 +202,7 @@ public class VirtualSponser extends Activity {
 //            return imageView;
 //        }
 //    }
+
 
 
 //    @Override
